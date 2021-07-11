f540dc57bfcb51efeade920e0fcfd7d950542aed
Andrea-pro <zadd@zilio.ch>
2021-07-11 13:43:38 +0200

    character controller w. basic movements

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{ 
+    private CharacterController _controller;
+    [SerializeField] private float _speed = 5f;
+    [SerializeField] private float _gravity = 1.0f;
+    [SerializeField] private float _jumpHeight = 25f;
+    [SerializeField] private float _yVelocity;  //cache y value of velocity
+    [SerializeField] Vector3 velocity;
+    private bool _jumpAgain = false; // _canDoubleJump
+
+    void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        Vector3 direction = new Vector3(horizontalInput, 0, 0);
+        Vector3 velocity = direction * _speed;
+
+        if(_controller.isGrounded == true)
+        {
+           if(Input.GetKeyDown(KeyCode.Space))
+            {
+                _yVelocity = _jumpHeight;
+                _jumpAgain = true;
+            }
+        }   
+        else
+        {
+           if (Input.GetKeyDown(KeyCode.Space))
+            {
+                if(_jumpAgain == true)
+                {
+                    _yVelocity += _jumpHeight;
+                    _jumpAgain = false;
+                }
+            } 
+
+            _yVelocity -= _gravity/3;  //solution for v.2020 /3
+        }
+
+        velocity.y = _yVelocity;
+        _controller.Move(velocity * Time.deltaTime);
+    }
+}
